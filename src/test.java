ddc4f929bd5b56081a62a1740ec4fd7b87f55642
arthur.edson <arthur.edson@hotmart.com>
2023-01-24 17:18:51 -0300

    revert

--- src/test.java
+++ src/test.java
@@ -4,6 +4,10 @@ public class test {
 
     public static void main(String[] args) {
         System.out.println("repo de test");
+
+        System.out.println("repo de test2");
+
+        System.out.println("repo de test3");
     }
 
 
