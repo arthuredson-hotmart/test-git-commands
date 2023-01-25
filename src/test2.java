00158ed9f954aae71bc9d6340ec8eb8ba3b5d50d
arthur.edson <arthur.edson@hotmart.com>
2023-01-25 09:00:17 -0300

    commit

---
+++ src/test2.java
@@ -0,0 +1,12 @@
+package src;
+
+public class test2 {
+
+    public static void main(String[] args) {
+        System.out.println("repo de test");
+
+        System.out.println("repo de test2");
+    }
+
+
+}
